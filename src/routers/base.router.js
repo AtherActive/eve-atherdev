e6c109cc597f49c4e22f30f9afb301db1551cee7
AtherActive <ikbensalty@gmail.com>
2022-09-12 10:56:40 +0200

    Final changes for now

--- src/routers/base.router.js
+++ src/routers/base.router.js
@@ -8,4 +8,8 @@ router.get('/', (req, res) => {
     res.render('./index.njk');
 })
 
+router.get('/credits',(req,res) => {
+    res.render('./credits.njk');
+})
+
 module.exports = router;
--- src/routers/rolling.router.js
+++ src/routers/rolling.router.js
@@ -25,4 +25,6 @@ router.get('/wh/:wormholeid', async (req, res) => {
     });
 })
 
+
+
 module.exports = router;
